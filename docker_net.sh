3f9ad8e3dbd5d178d8233c0839c33ebb2258d22c
benJamin <bzawodni@gmail.com>
2023-02-08 20:09:18 -0500

    create-a-node ability activation a go.

---
+++ docker_net.sh
@@ -0,0 +1,32 @@
+
+sudo nano /etc/ufw/after.rules
+
+# BEGIN UFW AND DOCKER
+*filter
+:ufw-user-forward - [0:0]
+:DOCKER-USER - [0:0]
+-A DOCKER-USER -j RETURN -s 10.0.0.0/8
+-A DOCKER-USER -j RETURN -s 172.16.0.0/12
+-A DOCKER-USER -j RETURN -s 192.168.0.0/16
+
+-A DOCKER-USER -j ufw-user-forward
+
+-A DOCKER-USER -j DROP -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -d 192.168.0.0/16
+-A DOCKER-USER -j DROP -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -d 10.0.0.0/8
+-A DOCKER-USER -j DROP -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -d 172.16.0.0/12
+-A DOCKER-USER -j DROP -p udp -m udp --dport 0:32767 -d 192.168.0.0/16
+-A DOCKER-USER -j DROP -p udp -m udp --dport 0:32767 -d 10.0.0.0/8
+-A DOCKER-USER -j DROP -p udp -m udp --dport 0:32767 -d 172.16.0.0/12
+
+-A DOCKER-USER -j RETURN
+COMMIT
+# END UFW AND DOCKER
+
+sudo systemctl restart ufw
+
+
+sudo docker container inspect volcy_1
+
+sudo ufw route allow proto tcp from any to 172.18.0.3 port 8090
+sudo ufw route allow proto tcp from any to 172.18.0.3 port 6000
+sudo ufw route allow proto tcp from any to 172.18.0.4 port 6000
---
+++ node-build.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+###################################
+# create the pool #
+###################################
+NODE_NAME=$1
+NETWORK=$2
+NODE_TYPE=$3
+POOL_NAME=$4
+
+###################################
+# To change the db path #
+###################################
+DB_DIR=/path/to/your/$NODE_NAME/db
+
+
+
+# Set the color variable
+green='\033[0;32m'
+# Clear the color after that
+clear='\033[0m'
+printf "${green} ************************************************* ${clear} \n"
+printf "*  Name:      ${green} ${NODE_NAME} ${clear} \n"
+printf "*  Network:   ${green} ${NETWORK} ${clear}\n"
+printf "*  Type:      ${green} ${NODE_TYPE} ${clear}\n"
+printf "*  Pool Name: ${green} ${POOL_NAME} ${clear}\n\n"
+printf "${green} ************************************************* ${clear} \n"
+
+if [ $NETWORK != "mainnet" ] && [ $NETWORK != "preprod" ]; then
+    printf "supported networks: preprod | mainnet\n"
+    exit 1
+fi
+
+./guild-deploy.sh -s f -t $NODE_NAME -n $NETWORK
+#./guild-deploy.sh -s pdlcx -t $NODE_NAME -n $NETWORK
+
+#cp /opt/cardano/cnode/files/topology.json /opt/cardano/$NODE_NAME/files/$NETWORK-topology.json
+#cp /opt/cardano/cnode/files/config.json /opt/cardano/$NODE_NAME/files/$NETWORK-config.json
+
+mkdir -pm777 nodes
+sudo docker stop $NODE_NAME
+sudo docker rm $NODE_NAME
+
+
+if [ $NETWORK == "preprod" ] && [ "$NODE_TYPE" == "relay" ]; then
+
+printf "${green}[Info] Creating preprod relay node${clear}\n"
+
+cp cfg/entrypoint.sh.$NETWORK.submit /opt/cardano/$NODE_NAME/files/entrypoint.sh
+
+cat > nodes/$NODE_NAME << EOF
+docker run -dit \
+--name $NODE_NAME \
+--security-opt=no-new-privileges \
+--cpus=3 \
+--net ezNet \
+--ip 172.18.0.4 \
+--entrypoint=/opt/cardano/cnode/files/entrypoint.sh \
+-e NETWORK=preprod \
+-e TOPOLOGY="/opt/cardano/cnode/files/$NETWORK-topology.json" \
+-e CONFIG="/opt/cardano/cnode/files/$NETWORK-config.json" \
+-e CPU_CORES=2 \
+-p 3000:6000 \
+-p 12799:12798 \
+-p 8091:8090 \
+-v /opt/cardano/$NODE_NAME/db:/opt/cardano/cnode/db \
+-v /opt/cardano/$NODE_NAME/files:/opt/cardano/cnode/files \
+cardanocommunity/cardano-node
+EOF
+
+cp cfg/$NETWORK-topology.json.relay.p2p /opt/cardano/$NODE_NAME/files/$NETWORK-topology.json
+cp cfg/$NETWORK-config.json.relay.p2p /opt/cardano/$NODE_NAME/files/$NETWORK-config.json
+
+fi
+
+if [ $NETWORK == "mainnet" ] && [ "$NODE_TYPE" == "relay" ]; then
+
+printf "${green}[Info] Creating mainnet relay node${clear}\n"
+
+cp cfg/entrypoint.sh.$NETWORK.submit /opt/cardano/$NODE_NAME/files/entrypoint.sh
+
+cat > nodes/$NODE_NAME << EOF
+docker run -dit \
+--name $NODE_NAME \
+--security-opt=no-new-privileges \
+--memory=25g \
+--net ezNet \
+--ip 172.18.0.3 \
+--entrypoint=/opt/cardano/cnode/files/entrypoint.sh \
+-e CPU_CORES=4 \
+-e NETWORK=mainnet \
+-e TOPOLOGY="/opt/cardano/cnode/files/$NETWORK-topology.json" \
+-e CONFIG="/opt/cardano/cnode/files/$NETWORK-config.json" \
+-e HOSTADDR=0.0.0.0 \
+-p 6000:6000 \
+-p 12798:12798 \
+-p 8090:8090 \
+-v /opt/cardano/$NODE_NAME/db:/opt/cardano/cnode/db \
+-v /opt/cardano/$NODE_NAME/files:/opt/cardano/cnode/files \
+cardanocommunity/cardano-node
+EOF
+
+fi
+
+if [ $NETWORK == "mainnet" ] && [ "$NODE_TYPE" == "dev" ]; then
+
+printf "${green}[Info] Creating mainnet relay node${clear}\n"
+#install -d -m 0755 -o <your_user> -g <your_group> $DB_DIR
+cp cfg/entrypoint.sh.$NETWORK.submit /opt/cardano/$NODE_NAME/files/entrypoint.sh
+
+cat > nodes/$NODE_NAME << EOF
+docker run -dit \
+--name $NODE_NAME \
+--security-opt=no-new-privileges \
+--memory=25g \
+--cpus=4 \
+--net ezNet \
+--ip 172.18.0.6 \
+--entrypoint=/opt/cardano/cnode/files/entrypoint.sh \
+-e NETWORK=mainnet \
+-e TOPOLOGY="/opt/cardano/cnode/files/$NETWORK-topology.json" \
+-e CONFIG="/opt/cardano/cnode/files/$NETWORK-config.json" \
+-e CPU_CORES=4 \
+-p 7000:6000 \
+-p 12796:12798 \
+-v $DB_DIR:/opt/cardano/cnode/db \
+-v /opt/cardano/$NODE_NAME/files:/opt/cardano/cnode/files \
+cardanocommunity/cardano-node
+EOF
+
+fi
+
+cp cfg/$NETWORK-topology.json.relay.p2p /opt/cardano/$NODE_NAME/files/$NETWORK-topology.json
+cp cfg/$NETWORK-config.json.relay.p2p /opt/cardano/$NODE_NAME/files/$NETWORK-config.json
+
+sudo chmod +x nodes/$NODE_NAME
+sudo ./nodes/$NODE_NAME
